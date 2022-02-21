a6a2d902b16abd4f15e0e90f876eaf3683179372
Euan Kemp <euank@euank.com>
2022-02-21 00:02:51 -0800

    Begin experimenting with qemu runner

--- .github/nixek-ci/machine.nix
+++ .github/nixek-ci/machine.nix
@@ -1,40 +1,69 @@
 { nixpkgs, pkgs }:
-  ((import "${nixpkgs}/nixos/release.nix") {
+let
+  lib = pkgs.lib;
+  evalConfig = import "${nixpkgs}/nixos/lib/eval-config.nix";
+  sharedConfig = config: {
+    environment.systemPackages = with pkgs; [
+      git
+      ddate
+    ];
+
+    users.users.nixek = {
+      description = "nixek ci user";
+      isNormalUser = true;
+      createHome = true;
+      home = "/home/nixek";
+      group = "nixek";
+      extraGroups = [ "wheel" ];
+      uid = 1000;
+      useDefaultShell = true;
+    };
+
+    security.sudo.extraConfig = ''
+        %wheel	ALL=(ALL) NOPASSWD: ALL
+    '';
+
+    # TODO: the overlay should just define a nixos service for this
+    systemd.services.nixekd = {
+      description = "Run nixekcid";
+      wantedBy = [ "multi-user.target" ];
+      serviceConfig = {
+        Type = "oneshot";
+        RemainAfterExit = true;
+        ExecStart = "${pkgs.nixekcid} run-job --user nixek";
+      };
+    };
+  };
+  amazonImage = ((import "${nixpkgs}/nixos/release.nix") {
     configuration = { config, ... }: {
       amazonImage = {
         format = "raw";
         sizeMB = 16 * 1024;
       };
-
-      environment.systemPackages = with pkgs; [
-        git
-        ddate
-      ];
-
-      users.users.nixek = {
-        description = "nixek ci user";
-        isNormalUser = true;
-        createHome = true;
-        home = "/home/nixek";
-        group = "nixek";
-        extraGroups = [ "wheel" ];
-        uid = 1000;
-        useDefaultShell = true;
-      };
-
-      security.sudo.extraConfig = ''
-        %wheel	ALL=(ALL) NOPASSWD: ALL
-      '';
-
-      # TODO: the overlay should just define a nixos service for this
-      systemd.services.nixekd = {
-        description = "Run nixekcid";
-        wantedBy = [ "multi-user.target" ];
-        serviceConfig = {
-          Type = "oneshot";
-          RemainAfterExit = true;
-          ExecStart = "${pkgs.nixekcid} run-job --user nixek";
-        };
-      };
+      modules = [ (sharedConfig { inherit config; }) ];
     };
-  }).amazonImage.x86_64-linux
+  }).amazonImage.x86_64-linux;
+
+  qemuImage = ((import "${nixpkgs}/nixos/lib/make-disk-image.nix") {
+    inherit pkgs lib;
+
+    diskSize = 40 * 1024;
+
+    config = (evalConfig {
+      system = "x86_64-linux";
+      modules = [
+        ({
+          imports = [
+            "${nixpkgs}/nixos/modules/virtualisation/qemu-vm.nix"
+            "${nixpkgs}/nixos/modules/profiles/qemu-guest.nix"
+          ];
+        })
+        sharedConfig
+      ];
+    }).config;
+  });
+in
+  {
+    aws = amazonImage;
+    qemu = qemuImage;
+  }
---
+++ run-locally2.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -eux -o pipefail
+
+# Build the machine image
+nix build --impure --expr 'with (builtins.getFlake (builtins.toString ./.)); (ci.jobs.demo-job { info = "TODO"; }).machine.qemu'
+
+# Upload the machine image
+echo "TODO"
+exit 1
+
+# Run CI, use SSH to monitor it
